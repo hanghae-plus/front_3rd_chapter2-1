9180345dbdd4ec4060325d2905824738c2873f41
soyoonJ <sytkfkd96@gmail.com>
2024-10-13 19:38:02 +0900

    refactor: main scheduleRandomSales 내 updateSelOpts 호출

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -64,6 +64,7 @@ const scheduleRandomSales = () => {
       if (Math.random() < 0.3 && luckyItem.quantity > 0) {
         luckyItem.price = Math.round(luckyItem.price * 0.8);
         alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
+        updateSelOpts();
       }
     }, 30000);
   }, Math.random() * 10000);
@@ -77,6 +78,7 @@ const scheduleRandomSales = () => {
         if (suggest) {
           alert(suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!');
           suggest.price = Math.round(suggest.price * 0.95);
+          updateSelOpts();
         }
       }
     }, 60000);
