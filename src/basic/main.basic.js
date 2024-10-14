79e5d31ed2ff7033349c7101d572f5a9c5af1e56
i33W <hwangbomee@kakao.com>
2024-10-15 02:15:59 +0900

    temp: 임시 저장

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -85,31 +85,31 @@ function main() {
 
   calcCart(sum);
 
-  //   setTimeout(function () {
-  //     setInterval(function () {
-  //       let luckyItem = prodList[Math.floor(Math.random() * prodList.length)];
-  //       if (Math.random() < 0.3 && luckyItem.q > 0) {
-  //         luckyItem.val = Math.round(luckyItem.val * 0.8);
-  //         alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
-  //         updateSelOpts(sel, prodList);
-  //       }
-  //     }, 30000);
-  //   }, Math.random() * 10000);
-  //
-  //   setTimeout(function () {
-  //     setInterval(function () {
-  //       if (lastSel) {
-  //         let suggest = prodList.find(function (item) {
-  //           return item.id !== lastSel && item.q > 0;
-  //         });
-  //         if (suggest) {
-  //           alert(suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!');
-  //           suggest.val = Math.round(suggest.val * 0.95);
-  //           updateSelOpts(sel, prodList);
-  //         }
-  //       }
-  //     }, 60000);
-  //   }, Math.random() * 20000);
+  setTimeout(function () {
+    setInterval(function () {
+      let luckyItem = prodList[Math.floor(Math.random() * prodList.length)];
+      if (Math.random() < 0.3 && luckyItem.q > 0) {
+        luckyItem.val = Math.round(luckyItem.val * 0.8);
+        alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
+        updateSelOpts(sel, prodList);
+      }
+    }, 30000);
+  }, Math.random() * 10000);
+
+  setTimeout(function () {
+    setInterval(function () {
+      if (lastSel) {
+        let suggest = prodList.find(function (item) {
+          return item.id !== lastSel && item.q > 0;
+        });
+        if (suggest) {
+          alert(suggest.name + '은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!');
+          suggest.val = Math.round(suggest.val * 0.95);
+          updateSelOpts(sel, prodList);
+        }
+      }
+    }, 60000);
+  }, Math.random() * 20000);
 }
 
 function updateSelOpts(sel, prodList) {
