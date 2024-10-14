4f072b4931007a85ad6421272a49dc5f159e945d
soyoonJ <sytkfkd96@gmail.com>
2024-10-15 02:25:52 +0900

    refactor: cart.js updateCartInfos 함수 calculateCartTotals 로직 분리

--- src/basic/utils/cart.js
+++ src/basic/utils/cart.js
@@ -6,13 +6,45 @@ import {
 } from './discount';
 import { products, renderProductsStockInfo } from './product';
 
-// TODO
 export const updateCartInfos = (bonusPoints) => {
+  const { totalItems, totalPrice, discountedTotalPrice } = calculateCartTotals();
+
+  const updatedTotalPriceAndDiscountRate = calculateTotalProductsBulkDiscount(
+    totalItems,
+    totalPrice,
+    discountedTotalPrice,
+  );
+  const { updatedTotalPrice, discountRate } = calculateDayDiscount(updatedTotalPriceAndDiscountRate);
+
+  updateCartTotalInfo(updatedTotalPrice, discountRate);
+  bonusPoints = updateBonusPoints(bonusPoints, updatedTotalPrice);
+  renderProductsStockInfo();
+
+  return bonusPoints;
+};
+export const updateCartItemInfo = (targetProduct, newItemQuantity, targetCartItem) => {
+  const newCartItemInfo = `${targetProduct.name} - ${targetProduct.price}원 x ${newItemQuantity}`;
+  targetCartItem.querySelector('span').textContent = newCartItemInfo;
+};
+const updateCartTotalInfo = (discountedTotalPrice, discountRate) => {
+  const cartTotalInfo = document.getElementById('cart-total');
+
+  cartTotalInfo.textContent = `총액: ${Math.round(discountedTotalPrice)}원`;
+  if (discountRate > 0) {
+    const span = document.createElement('span');
+    span.className = 'text-green-500 ml-2';
+    span.textContent = `(${(discountRate * 100).toFixed(1)}% 할인 적용)`;
+    cartTotalInfo.appendChild(span);
+  }
+};
+
+const calculateCartTotals = () => {
   let totalItems = 0;
   let totalPrice = 0;
   let discountedTotalPrice = 0;
 
   const cartItems = document.getElementById('cart-items').children;
+
   for (let cartItem of cartItems) {
     const currentProduct = products.find((product) => product.id === cartItem.id);
     const quantity = parseInt(cartItem.querySelector('span').textContent.split('x ')[1]);
@@ -25,35 +57,8 @@ export const updateCartInfos = (bonusPoints) => {
     discountedTotalPrice += calculateDiscountedPrice(productTotalPrice, productBulkDiscountRate);
   }
 
-  const updatedTotalPriceAndDiscountRate = calculateTotalProductsBulkDiscount(
-    totalItems,
-    totalPrice,
-    discountedTotalPrice,
-  );
-  const { updatedTotalPrice, discountRate } = calculateDayDiscount(updatedTotalPriceAndDiscountRate);
-
-  updateTotalInfo(updatedTotalPrice, discountRate);
-  bonusPoints = updateBonusPoints(bonusPoints, updatedTotalPrice);
-  renderProductsStockInfo();
-
-  return bonusPoints;
+  return { totalItems, totalPrice, discountedTotalPrice };
 };
-export const updateCartItemInfo = (targetProduct, newItemQuantity, targetCartItem) => {
-  const newCartItemInfo = `${targetProduct.name} - ${targetProduct.price}원 x ${newItemQuantity}`;
-  targetCartItem.querySelector('span').textContent = newCartItemInfo;
-};
-export const updateTotalInfo = (discountedTotalPrice, discountRate) => {
-  const cartTotalInfo = document.getElementById('cart-total');
-
-  cartTotalInfo.textContent = '총액: ' + Math.round(discountedTotalPrice) + '원';
-  if (discountRate > 0) {
-    const span = document.createElement('span');
-    span.className = 'text-green-500 ml-2';
-    span.textContent = '(' + (discountRate * 100).toFixed(1) + '% 할인 적용)';
-    cartTotalInfo.appendChild(span);
-  }
-};
-
 const createBonusPointsTag = () => {
   const pointTagElement = document.createElement('span');
   pointTagElement.id = 'loyalty-points';
