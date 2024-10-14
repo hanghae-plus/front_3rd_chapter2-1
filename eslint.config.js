596e54737ac9617c5f08062960907e3faa6fc656
i33W <hwangbomee@kakao.com>
2024-10-14 21:43:11 +0900

    chore: eslint 규칙 추가

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,15 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
+  pluginJs.configs.recommended,
   {
     languageOptions: { globals: globals.browser },
+    rules: {
+      eqeqeq: 'error',
+      'no-console': 'warn',
+      curly: 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-var': 'error',
+    },
   },
-  pluginJs.configs.recommended,
 ];
