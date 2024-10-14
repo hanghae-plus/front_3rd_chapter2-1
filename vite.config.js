d78ba8f856d4bc95b01e7200e653ada093476ad4
KimDanwoo <dudgh9198@naver.com>
2024-10-14 21:19:42 +0900

    chore: 프로젝트 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    setupFiles: './src/setupTests.js',
+  },
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+    },
   },
 })
