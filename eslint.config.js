77039ba5188c56c4539e03f1c415e6117cb61349
이동아 <christar99@naver.com>
2024-10-15 00:59:20 +0900

    프로젝트 기본 셋팅 - 프리티어, eslint설치 & 설정

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": ["error"], // 사용되지 않는 변수 검출
+      "no-console": ["warn"], // console.log() 사용 제한
+      eqeqeq: ["error", "always"], // === 및 !== 연산자 사용 강제
+      "prefer-const": ["error"], // 재할당되지 않는 변수에 const 사용 강제
+      "no-var": ["error"], // var 대신 let 또는 const 사용 강제
+      curly: ["error", "all"], // 모든 제어문에 중괄호 사용 강제(화살표함수에 영향 미치지 않음)
+      "arrow-body-style": ["error", "as-needed"], // 화살표 함수 단일표현식 중괄호 삭제
+      "no-shadow": "error", // 변수 섀도잉 방지(상위스코프와 동일한 하위스코프의 변수 네이밍)
+      "no-magic-numbers": ["warn", { ignore: [0, 1] }], // 매직넘버 사용금지
+      "no-multi-spaces": ["error"], // 여러 개의 연속된 공백 금지
+      "space-before-function-paren": ["error", "never"], // 함수 앞 공백 정의
+    },
+  },
+];
