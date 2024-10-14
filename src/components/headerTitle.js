a14a2633286ce5771f45a84ef901cd672e3e325c
Gred <jihong.park.dev@gmail.com>
2024-10-14 22:47:11 +0900

    chore: main.js 주석 추가

---
+++ src/components/headerTitle.js
@@ -0,0 +1,7 @@
+export const headerTitle = () => {
+  return `
+    <h1 class="text-2xl font-bold mb-4">
+        장바구니
+    </h1>
+    `;
+};
---
+++ src/components/productSelect.js
@@ -0,0 +1,16 @@
+// TODO
+// Component는 default export
+// arrow function으로 만든다.
+// updateSelOpts() 로직을 컴포넌트에서 구현한다.
+const ProductSelect = () => {};
+
+export default ProductSelect;
+
+// 8조 rule
+// 함수 선언은 arrow function 으로 통일
+// 함수 이름은 각자 짓고 다음 코어타임때 맞춰보기 (제일 좋은걸로 통일)
+// 변수명을 풀어쓰기
+// 변수명은 카멜케이스
+// 컴포넌트는 파스칼 케이스
+// 상수는 대문자_어쩌고_로_선언하기
+// var은 const, let 으로 변경하기
