289bd59a3368d5189c77a708e86b933fc6f277b0
i33W <hwangbomee@kakao.com>
2024-10-15 03:11:00 +0900

    fix: eslint 속성 에러 수정

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,14 @@ export default [
   pluginJs.configs.recommended,
   {
     languageOptions: { globals: globals.browser },
+    ignores: ['node_modules/', 'src/main.js'],
     rules: {
       eqeqeq: 'error',
       'no-console': 'warn',
       curly: 'error',
       // 'comma-dangle': ['error', 'always-multiline'],
       'no-var': 'error',
+      'no-undefined': 'error',
     },
-    ignorePatterns: ['node_modules/', 'src/main.js'],
   },
 ];
