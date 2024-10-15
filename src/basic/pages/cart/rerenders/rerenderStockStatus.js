79ea2581aa2e3259d487bf15fd1a96c7be9bc476
윤성용 <iop017779@gmail.com>
2024-10-15 20:22:12 +0900

    refactor : rerenderStockStatus 함수 변수 네이밍 수정 및 텍스트 업데이트 로직 수정

--- src/basic/pages/cart/rerenders/rerenderStockStatus.js
+++ src/basic/pages/cart/rerenders/rerenderStockStatus.js
@@ -5,25 +5,24 @@ const createStockStatusText = (item) =>
   `${item.name}: ${item.quantity > 0 ? '재고 부족' + ` (${item.quantity}개 남음)` : '품절'}\n`;
 
 export function rerenderStockStatus() {
-  let updateContent = '';
-  let currentContent = '';
+  let stockStatusText = '';
 
   const { cartItems } = cartItemState.getState();
   const $stockInfoContainer = document.getElementById('stock-status');
 
-  const notQuantityList = DEFAULT_PRODUCT_LIST.filter((product) => product.quantity === 0);
+  const soldOutItems = DEFAULT_PRODUCT_LIST.filter((product) => product.quantity === 0);
 
-  if (notQuantityList.length > 0) {
-    notQuantityList.forEach((item) => {
-      currentContent = createStockStatusText(item);
+  if (soldOutItems.length > 0) {
+    soldOutItems.forEach((item) => {
+      stockStatusText += createStockStatusText(item);
     });
   }
 
   cartItems.forEach((cartItem) => {
     if (cartItem.quantity <= 5) {
-      updateContent = createStockStatusText(cartItem);
+      stockStatusText += createStockStatusText(cartItem);
     }
   });
 
-  $stockInfoContainer.textContent = currentContent + updateContent;
+  $stockInfoContainer.textContent = stockStatusText;
 }
