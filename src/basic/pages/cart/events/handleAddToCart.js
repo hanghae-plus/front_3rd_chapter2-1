7949efa8d9b3a41871710345cf1f3f6054f91050
윤성용 <iop017779@gmail.com>
2024-10-15 21:19:31 +0900

    refactor : handleAddToCart 함수 네이밍 수정

--- src/basic/pages/cart/events/handleAddToCart.js
+++ src/basic/pages/cart/events/handleAddToCart.js
@@ -3,23 +3,23 @@ import { updateCartItemQuantity } from '../modules/updateCartItemQuantity.js';
 import { calculateCartTotals } from '../modules/calculateCartTotals.js';
 import { DEFAULT_PRODUCT_LIST } from '../constant/defaultProducts.js';
 
-function updateCart(product) {
+function addItemToCart(selectedProduct) {
   const { cartItems } = cartItemState.getState();
   const setCartItemState = cartItemState.setState;
-  const currentProduct = cartItems.find((item) => item.id === product.id);
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === selectedProduct.id);
 
-  if (product.quantity === 0) {
+  if (selectedProduct.quantity === 0) {
     return;
   }
 
-  if (currentProduct) {
-    updateCartItemQuantity(product.id, 1);
+  if (existingCartItem) {
+    updateCartItemQuantity(selectedProduct.id, 1);
   } else {
     setCartItemState((prevState) => {
       return {
         cartItems: [
           ...prevState.cartItems,
-          { ...product, quantity: product.quantity - 1, selectQuantity: 1 },
+          { ...selectedProduct, quantity: selectedProduct.quantity - 1, selectQuantity: 1 },
         ],
       };
     });
@@ -29,12 +29,12 @@ function updateCart(product) {
 }
 
 export function handleAddToCart() {
-  const setSelectedItemState = selectedProductItemState.setState;
-  const selectedProduct = document.getElementById('product-select');
+  const setSelectedProductState = selectedProductItemState.setState;
+  const $productSelectElement = document.getElementById('product-select');
   const selectedProductItem = DEFAULT_PRODUCT_LIST.find(
-    (product) => product.id === selectedProduct.value
+    (product) => product.id === $productSelectElement.value
   );
 
-  updateCart(selectedProductItem);
-  setSelectedItemState({ selectedProductItem: selectedProductItem });
+  addItemToCart(selectedProductItem);
+  setSelectedProductState({ selectedProductItem: selectedProductItem });
 }
