737ba38817a083130df40521a9bc8110f391d911
남균 <gyuni9604@naver.com>
2024-10-16 01:17:07 +0900

    📝 fix : 수정 사항 적용

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,5 +1,25 @@
-function main() {
+import { initCart, handleClickAddToCart, calculatorCart } from "./utils"
+import { initCartInfo } from "./state"
 
+const main = () => {
+  // cartInfo 초기화
+  initCartInfo()
+  // 레이아웃 초기화
+  initCart()
+  // 장바구니 계산
+  calculatorCart()
+  // 이벤트 리스너 등록
+  setEventListeners()
+
+  //TODO : 랜덤 할인 이벤트 추가
+  //TODO : 품절 업데이트 추가  || as-is : updateStockInfo
+  //TODO : 할인 상수 추가
 }
 
-main();
+// 이벤트 리스너 등록
+const setEventListeners = () => {
+  document.querySelector("#add-to-cart").addEventListener("click", handleClickAddToCart)
+  //TODO : 카트 아이템 클릭시 이벤트 추가
+}
+
+main()
