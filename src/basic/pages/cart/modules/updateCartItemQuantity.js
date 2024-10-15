91c790a25bd5221419c5db1093fce337bdda7c2a
윤성용 <iop017779@gmail.com>
2024-10-15 21:38:06 +0900

    refactor : updateCartItemQuantity 함수 변수 네이밍 수정

--- src/basic/pages/cart/modules/updateCartItemQuantity.js
+++ src/basic/pages/cart/modules/updateCartItemQuantity.js
@@ -5,18 +5,18 @@ export function updateCartItemQuantity(productId, quantityChange) {
   const { cartItems } = cartItemState.getState();
   const setCartItemState = cartItemState.setState;
 
-  const prevProduct = DEFAULT_PRODUCT_LIST.find((item) => item.id === productId);
+  const defaultProductItem = DEFAULT_PRODUCT_LIST.find((item) => item.id === productId);
   const selectedCartItem = cartItems.find((item) => item.id === productId);
 
-  const newSelectQuantity = selectedCartItem.selectQuantity + quantityChange;
-  const newQuantity = selectedCartItem.quantity - quantityChange;
+  const updatedSelectQuantity = selectedCartItem.selectQuantity + quantityChange;
+  const updatedAvailableQuantity = selectedCartItem.quantity - quantityChange;
 
-  if (newSelectQuantity > prevProduct.quantity) {
+  if (updatedSelectQuantity > defaultProductItem.quantity) {
     alert('재고가 부족합니다.');
     return;
   }
 
-  if (newSelectQuantity === 0) {
+  if (updatedSelectQuantity === 0) {
     setCartItemState((prevState) => {
       const updatedCartItems = prevState.cartItems.filter((item) => item.id !== productId);
 
@@ -29,8 +29,8 @@ export function updateCartItemQuantity(productId, quantityChange) {
       if (item.id === productId) {
         return {
           ...item,
-          quantity: newQuantity,
-          selectQuantity: newSelectQuantity,
+          quantity: updatedAvailableQuantity,
+          selectQuantity: updatedSelectQuantity,
         };
       }
       return item;
