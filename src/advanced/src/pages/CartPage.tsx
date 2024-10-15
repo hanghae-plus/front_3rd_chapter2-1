da99f518843c6d79a74967662067a0fe8842bbe8
soyoonJ <sytkfkd96@gmail.com>
2024-10-16 02:12:53 +0900

    refactor: 기본과제 scheduleRandomDiscount => useAlertDiscount 훅 생성

--- src/advanced/src/pages/CartPage.tsx
+++ src/advanced/src/pages/CartPage.tsx
@@ -2,8 +2,11 @@ import CartItems from '../components/CartItems';
 import CartTotalPrice from '../components/CartTotalPrice';
 import ProductSelect from '../components/ProductSelect';
 import Title from '../components/Title';
+import useAlertDiscount from '../hooks/useAlertDiscount';
 
 const CartPage = () => {
+  useAlertDiscount();
+
   return (
     <main className="bg-gray-100 p-8">
       <section className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
