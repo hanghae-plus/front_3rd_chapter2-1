a49f69bd603d6f814bd5ccb0a2e7e4395e5916aa
정원 <jwha94@gmail.com>
2024-10-17 00:16:41 +0900

    feat: CartTotal 컴포넌트 추가

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,3 +1,17 @@
+import { useState } from 'react';
+import { CartTotal } from './components/CartTotal';
+import { DEFAULT_CART_TOTAL, CartTotal as TCartTotal } from './model/cartTotal';
+
 export const App = () => {
-  return <div>Hello World</div>;
+  const [cartTotal, setCartTotal] = useState<TCartTotal>(DEFAULT_CART_TOTAL);
+
+  return (
+    <div className="bg-gray-100 p-8">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
+        <h1 className="text-2xl font-bold mb-4">장바구니</h1>
+
+        <CartTotal cartTotal={cartTotal} />
+      </div>
+    </div>
+  );
 };
---
+++ src/advanced/model/cartTotal.ts
@@ -0,0 +1,11 @@
+export type CartTotal = {
+  totalPrice: number;
+  discountRate: number;
+  point: number;
+};
+
+export const DEFAULT_CART_TOTAL = {
+  discountRate: 0,
+  point: 0,
+  totalPrice: 0,
+};
