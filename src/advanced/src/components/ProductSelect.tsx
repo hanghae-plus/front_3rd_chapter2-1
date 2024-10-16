052b4619a59497c5f76926bf3c9465a3ce93a4bc
soyoonJ <sytkfkd96@gmail.com>
2024-10-17 03:22:52 +0900

    fix: 품절상품 disabled

--- src/advanced/src/components/ProductSelect.tsx
+++ src/advanced/src/components/ProductSelect.tsx
@@ -14,7 +14,7 @@ const ProductSelect = () => {
         className="border rounded p-2 mr-2"
       >
         {products.map((product) => (
-          <option key={product.id} value={product.id}>
+          <option key={product.id} value={product.id} disabled={product.quantity === 0}>
             {product.name} - {product.price}원
           </option>
         ))}
