f8b5e6ad09d8c64d7245d28eb254ea09bd7ce628
KimMyeongSu <kms930508@gmail.com>
2024-10-16 14:54:10 +0900

    함수 분할, 변수명 수정

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,18 +1,3 @@
-// 개선 순서
-//1. 의미 있는 이름으로 개선하기
-//2. 함수 분리 (Extract Function)
-//3. 매직 넘버 제거 (Replace Magic Numbers)
-//4. 중복된 코드 제거
-//5. 조건문 간소화 (Simplify Conditionals)
-
-const DISCOUNT_PROBABILITY = 0.3;
-const LIGHTNING_DISCOUNT = 0.2;
-const BULK_DISCOUNT_LIMIT = 30;
-const TUESDAY_DISCOUNT = 0.1;
-
-function main() {}
-
-main();
 // 전역 변수
 let productList,
   productSelect,
