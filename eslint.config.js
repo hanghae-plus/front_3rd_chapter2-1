2496f969140ee9ddce96a8240f760b49eee13ca2
i33W <hwangbomee@kakao.com>
2024-10-17 01:02:28 +0900

    chore :: eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,14 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
+  { ignores: ['node_modules/', 'src/main.js', '**/__tests__/'] },
   pluginJs.configs.recommended,
   {
     languageOptions: { globals: globals.browser },
-    ignores: ['node_modules/', 'src/main.js'],
     rules: {
       eqeqeq: 'error',
       'no-console': 'warn',
-      curly: ['error', 'multi'],
-      // 'comma-dangle': ['error', 'always-multiline'],
+      curly: ['error', 'multi-or-nest'],
       'no-var': 'error',
       'no-undefined': 'error',
     },
