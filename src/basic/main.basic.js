7cef6bd1c9649789b66c12372cd6e4bf3be98414
남균 <gyuni9604@naver.com>
2024-10-16 23:09:14 +0900

    ♻️ refactor : 리펙토링 적용

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,25 +1,30 @@
-import { initCart, handleClickAddToCart, calculatorCart } from "./utils"
-import { initCartInfo } from "./state"
+import {
+  initCart,
+  handleClickAddToCart,
+  calculatorCart,
+  handleClickItem,
+  scheduleDiscount,
+  updateSelectOptions,
+} from './utils';
+import { initCartInfo } from './state';
 
 const main = () => {
   // cartInfo 초기화
-  initCartInfo()
+  initCartInfo();
   // 레이아웃 초기화
-  initCart()
+  initCart();
   // 장바구니 계산
-  calculatorCart()
+  calculatorCart();
+  // 할인 이벤트 스케줄링
+  scheduleDiscount();
   // 이벤트 리스너 등록
-  setEventListeners()
-
-  //TODO : 랜덤 할인 이벤트 추가
-  //TODO : 품절 업데이트 추가  || as-is : updateStockInfo
-  //TODO : 할인 상수 추가
-}
+  setEventListeners();
+};
 
 // 이벤트 리스너 등록
 const setEventListeners = () => {
-  document.querySelector("#add-to-cart").addEventListener("click", handleClickAddToCart)
-  //TODO : 카트 아이템 클릭시 이벤트 추가
-}
+  document.querySelector('#add-to-cart').addEventListener('click', handleClickAddToCart);
+  document.querySelector('#cart-items').addEventListener('click', handleClickItem);
+};
 
-main()
+main();
