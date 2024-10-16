f454a2c3e3984aed309b1510722d5f86ce40330d
최아람 <aarmakrr@hanmail.net>
2024-10-16 23:14:46 +0900

    [chore]: react, react-dom, vite plugin 등 리액트와 관련된 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    setupFiles: './src/setupTests.js',
   },
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]],
+      },
+      include: '**/*.js',
+    }),
+  ],
+});
