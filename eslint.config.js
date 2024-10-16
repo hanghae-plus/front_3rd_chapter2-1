227d6ebe234ef1600269aea7680934a4b44dd11a
남균 <gyuni9604@naver.com>
2024-10-16 23:36:42 +0900

    📝 fix : lint ,format 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslintParser from '@typescript-eslint/parser';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: typescriptEslintParser,
+    },
+    plugins: {
+      react: eslintPluginReact,
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': typescriptEslintPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Prettier 규칙을 에러로 처리
+      'react/react-in-jsx-scope': 'off', // React 17부터 JSX 자동 import
+      'no-unused-vars': 'warn', // 사용되지 않는 변수 경고
+      '@typescript-eslint/no-unused-vars': ['warn'], // TypeScript의 unused 변수 경고
+    },
+    settings: {
+      react: {
+        version: 'detect', // React 버전 자동 감지
+      },
+    },
+    ignores: ['node_modules/**'], // node_modules 폴더 무시
+  },
 ];
