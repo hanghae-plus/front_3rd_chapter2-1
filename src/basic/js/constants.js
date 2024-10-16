2f063124720eb052c73841426c7a1c82cf504c09
dongjang <jdwls27@naver.com>
2024-10-16 18:24:59 +0900

    constants.js 수정

--- src/basic/js/constants.js
+++ src/basic/js/constants.js
@@ -1,3 +1,11 @@
+const PRODUCT_LIST = [
+  { id: 'p1', name: '상품1', price: 10000, stock: 50 },
+  { id: 'p2', name: '상품2', price: 20000, stock: 30 },
+  { id: 'p3', name: '상품3', price: 30000, stock: 20 },
+  { id: 'p4', name: '상품4', price: 15000, stock: 0 },
+  { id: 'p5', name: '상품5', price: 25000, stock: 10 },
+];
+
 const DISCOUNT_PRODUCT_COUNT = 10;
 
 const DISCOUNT_5_PERCENT = 0.05;
@@ -5,8 +13,7 @@ const DISCOUNT_10_PERCENT = 0.1;
 const DISCOUNT_15_PERCENT = 0.15;
 const DISCOUNT_20_PERCENT = 0.2;
 const DISCOUNT_25_PERCENT = 0.25;
-
-const discountRates = {
+const DISCOUNT_RATES = {
   p1: DISCOUNT_10_PERCENT,
   p2: DISCOUNT_15_PERCENT,
   p3: DISCOUNT_20_PERCENT,
@@ -18,13 +25,14 @@ const DISCOUNT_25_PERCENT_PRODUCT_COUNT = 30;
 const ALERT_SHORT_STOCK = '재고가 부족합니다.';
 
 export {
+  PRODUCT_LIST,
   DISCOUNT_PRODUCT_COUNT,
   DISCOUNT_5_PERCENT,
   DISCOUNT_10_PERCENT,
   DISCOUNT_15_PERCENT,
   DISCOUNT_20_PERCENT,
   DISCOUNT_25_PERCENT,
-  discountRates,
+  DISCOUNT_RATES,
   DISCOUNT_25_PERCENT_PRODUCT_COUNT,
   ALERT_SHORT_STOCK,
 };
