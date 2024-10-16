7044f29d1fef6b00d13ecdf85d7184077621d98d
정원 <jwha94@gmail.com>
2024-10-16 22:57:41 +0900

    config: React 세팅

---
+++ src/advanced/main.advanced.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('app'));
+root.render(<Main />);
+
+function Main() {
+  return <h1>Hello, world</h1>;
+}
