9a81f78357b2f8b3da54d5d38fd92fa176b1c6f3
soyoonJ <sytkfkd96@gmail.com>
2024-10-16 20:46:35 +0900

    refactor: useProductSelect.tsx hooks 추가

--- src/advanced/src/components/ProductSelect.tsx
+++ src/advanced/src/components/ProductSelect.tsx
@@ -1,61 +1,7 @@
-import { ChangeEvent, useState } from 'react';
-import { products } from '../data/products';
-import { useStore } from '../stores/store';
-import { IProduct } from '../types/cart';
+import useProductSelect from '../hooks/useProductSelect';
 
 const ProductSelect = () => {
-  //   let bonusPoints = 0;
-  //   let lastAddedProduct = '';
-
-  const cartItems = useStore((state) => state.cartItems);
-  const updateCartItems = useStore((state) => state.updateCartItems);
-
-  const addToCart = (targetProduct: IProduct) => {
-    const currentCartItem = cartItems.find((item) => item.id === targetProduct.id);
-
-    if (currentCartItem) {
-      const updatedCartItems = cartItems.map((item) => {
-        if (item.id === targetProduct.id) {
-          return { ...item, quantity: item.quantity - 1, cartQuantity: item.cartQuantity + 1 };
-        } else return item;
-      });
-      updateCartItems(updatedCartItems);
-    } else {
-      updateCartItems([...cartItems, { ...targetProduct, cartQuantity: 1, quantity: targetProduct.quantity - 1 }]);
-    }
-
-    // const currentItemQuantity = getTargetItemElementQuantity($targetCartItem);
-    // const newItemQuantity = currentItemQuantity + 1;
-    // BUG: 로직 개선
-    // const isStockRemain = newItemQuantity <= targetProduct.quantity;
-    // if (isStockRemain) {
-    //   renderCartItemInfo(targetProduct, newItemQuantity, $targetCartItem);
-    //   targetProduct.quantity--;
-    // } else {
-    //   alert('재고가 부족합니다.');
-    // }
-  };
-
-  const handleAddToCart = () => {
-    const targetProduct = products.find((product) => {
-      return product.id === selected;
-    });
-
-    if (!targetProduct) return; // 상품 자체가 존재하지 않음
-
-    addToCart(targetProduct);
-    // if (targetProduct && targetProduct.quantity > 0) {
-    //   // const $targetCartItem = document.getElementById(targetProduct.id);
-    //   addToCart($targetCartItem, targetProduct);
-    //   // bonusPoints = updateCartInfos(bonusPoints);
-    //   // lastAddedProduct = selectedProductId;
-    // }
-  };
-
-  const [selected, setSelected] = useState('p1');
-  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelected(e.target.value);
-  };
+  const { selected, handleSelect, handleAddToCart } = useProductSelect();
 
   return (
     <>
---
+++ src/advanced/src/hooks/useProductSelect.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, useState } from 'react';
+import { products } from '../data/products';
+import { useStore } from '../stores/store';
+import { ICartItem, IProduct } from '../types/cart';
+
+const useProductSelect = () => {
+  const [selected, setSelected] = useState('p1');
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
+
+  const cartItems = useStore((state) => state.cartItems);
+  const updateCartItems = useStore((state) => state.updateCartItems);
+
+  const addToCart = (currentCartItem: ICartItem | undefined, targetProduct: IProduct) => {
+    if (!currentCartItem) {
+      updateCartItems([...cartItems, { ...targetProduct, cartQuantity: 1, quantity: targetProduct.quantity - 1 }]);
+      return;
+    }
+
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === targetProduct.id) {
+        return { ...item, quantity: item.quantity - 1, cartQuantity: item.cartQuantity + 1 };
+      } else return item;
+    });
+    updateCartItems(updatedCartItems);
+  };
+
+  const handleAddToCart = () => {
+    const targetProduct = products.find((product) => {
+      return product.id === selected;
+    });
+
+    if (!targetProduct) return; // 상품 자체가 존재하지 않음
+
+    const currentCartItem = cartItems.find((item) => item.id === targetProduct.id);
+
+    if (currentCartItem && currentCartItem.quantity === 0) {
+      alert('재고가 부족합니다.');
+      return;
+    }
+
+    addToCart(currentCartItem, targetProduct);
+  };
+
+  return { selected, handleSelect, handleAddToCart };
+};
+
+export default useProductSelect;
