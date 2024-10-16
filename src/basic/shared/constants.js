66f0be793093260cfac9e613d4b692105ee75666
John-Kim93 <dhkdhd123@gmail.com>
2024-10-17 00:38:43 +0900

    refactor : basic test

---
+++ src/basic/shared/constants.js
@@ -0,0 +1,6 @@
+export const ENTIRE_DISCOUNT_RATE = 0.75;
+export const DAY_DISCOUNT_RATE = 0.9;
+export const ONE_SECOND = 1000;
+export const ONE_MINUTE = 60 * 1000;
+export const INCREASE = 1;
+export const DECREASE = -1;
