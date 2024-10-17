4bc40ed736721d7fb272dbe7d4710bf967505ce2
JeongWu-Jane <park96100331@gmail.com>
2024-10-17 21:25:22 +0900

    ts 설치

--- src/advanced/main.advanced.tsx
+++ src/advanced/main.advanced.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('app'));
-
+const root = createRoot(document.getElementById('app')!);
 root.render(<Main />);
 
 function Main() {
