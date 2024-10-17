114ed102f3fd4ff15f38ccad4b5f28345eff3c5f
남균 <gyuni9604@naver.com>
2024-10-17 22:29:02 +0900

    📝 fix : 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugin: [react(), eslintPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
+    setupFiles: './src/setupTests.js'
   },
-});
+})
