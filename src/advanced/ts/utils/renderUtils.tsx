dcfd5513ccae6e9d749e65abf6c29c6b923c67af
JANG DONG JIN <jdwls27@naver.com>
2024-10-18 02:43:15 +0900

    renderUtils.tsx 정의

---
+++ src/advanced/ts/utils/renderUtils.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useAppContext } from '../../context/appContext';
+
+const useRenderUtils = () => {
+  const { productList, setBonusPointsSpan } = useAppContext();
+
+  //select 상품 리스트 렌더링
+  const renderProductList = () => {
+    return productList.map((product) => (
+      <option key={product.id} value={product.id} disabled={product.stock === 0 ? true : false}>
+        {`${product.name} - ${product.price}원 재고 :${product.stock}`}
+      </option>
+    ));
+  };
+
+  //보너스 포인트 세팅
+  const renderBonusPoints = (totalAmount: string) => {
+    setBonusPointsSpan(totalAmount);
+  };
+
+  return { renderProductList, renderBonusPoints };
+};
+
+export { useRenderUtils };
