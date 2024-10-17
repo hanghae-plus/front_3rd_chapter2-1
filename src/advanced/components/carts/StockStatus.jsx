4b1e77242e3ea878d8b35f4cf11810c7e8a93e13
Ensil-dev <lasags@naver.com>
2024-10-18 02:10:24 +0900

    feature: 상품 재고상태 표시 UI 구현

--- src/advanced/components/carts/StockStatus.jsx
+++ src/advanced/components/carts/StockStatus.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
-export const StockStatus = () => {
+export const StockStatus = ({ productInventory }) => {
+  console.log(productInventory);
+
   return (
     <div id="stock-status" className="mt-2 text-sm text-gray-500">
-      상품4: 품절
+      {productInventory.map((product) => {
+        if (product.quantity === 0) return `${product.name}: 품절`;
+        if (product.quantity <= 5) return `${product.name}: 재고 부족 (${product.quantity}개 남음)`;
+      })}
     </div>
   );
 };
