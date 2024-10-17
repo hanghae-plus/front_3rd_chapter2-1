60038bc3c5c93a3832b8ddcb8642692f2b74f830
JANG DONG JIN <jdwls27@naver.com>
2024-10-18 03:39:10 +0900

    CartAmountInfo 컴포넌트 추가

---
+++ src/advanced/components/CartAmountInfo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAppContext } from '../context/appContext';
+
+const CartAmountInfo = () => {
+  const { productSum, discountSpan, bonusPointsSpan } = useAppContext();
+  return (
+    <div>
+      <div id="cart-total" className="text-xl font-bold my-4">
+        {productSum}
+        <span className="text-green-500 ml-2">{discountSpan}</span>
+        <span id="loyalty-points" className="text-blue-500 ml-2">
+          {bonusPointsSpan}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartAmountInfo;
