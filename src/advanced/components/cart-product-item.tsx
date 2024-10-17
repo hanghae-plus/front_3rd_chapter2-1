d36a5e361699a58d08446f513b34ab7299b3a5ac
이동아 <christar99@naver.com>
2024-10-18 01:15:26 +0900

    장바구니 상품 개별 컴포넌트 추가(증감, 삭제기능 구현)

---
+++ src/advanced/components/cart-product-item.tsx
@@ -0,0 +1,74 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { CartProductType } from "./product-container";
+
+interface CartProductItemProps {
+  cartProduct: CartProductType;
+  setCartProductList: Dispatch<SetStateAction<CartProductType[]>>;
+}
+
+const CartProductItem = ({ cartProduct, setCartProductList }: CartProductItemProps) => {
+  const cartProductText = `${cartProduct.name} - ${cartProduct.price} x ${cartProduct.count}`;
+  const INCREASE_COUNT = 1;
+  const DECREASE_COUNT = -1;
+
+  const setCartProductCount = (prev: CartProductType[], quantity: number) => {
+    const productIndex = prev.findIndex(product => product.id === cartProduct.id);
+    return prev.with(productIndex, { ...cartProduct, count: cartProduct.count + quantity });
+  };
+
+  const removeCartProduct = (prev: CartProductType[]) =>
+    prev.filter(product => product.id !== cartProduct.id);
+
+  const decreaseCount = () => {
+    if (cartProduct.count === 1) {
+      removeProduct();
+    } else {
+      setCartProductList(prev => setCartProductCount(prev, DECREASE_COUNT));
+    }
+  };
+
+  const increaseCount = () => {
+    if (cartProduct.count === cartProduct.quantity) {
+      alert("재고가 부족합니다!");
+    } else {
+      setCartProductList(prev => setCartProductCount(prev, INCREASE_COUNT));
+    }
+  };
+
+  const removeProduct = () => {
+    setCartProductList(prev => removeCartProduct(prev));
+  };
+
+  return (
+    <div id={cartProduct.id} className="flex justify-between items-center mb-2">
+      <span>{cartProductText}</span>
+      <div>
+        <button
+          className="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1"
+          data-product-id={cartProduct.id}
+          data-change="-1"
+          onClick={decreaseCount}
+        >
+          -
+        </button>
+        <button
+          className="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1"
+          data-product-id={cartProduct.id}
+          data-change="1"
+          onClick={increaseCount}
+        >
+          +
+        </button>
+        <button
+          className="remove-item bg-red-500 text-white px-2 py-1 rounded"
+          data-product-id={cartProduct.id}
+          onClick={removeProduct}
+        >
+          삭제
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartProductItem;
