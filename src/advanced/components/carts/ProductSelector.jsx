7a9f81554ca5550b1f6fe8c6f514e80a5a95d1e6
Ensil-dev <lasags@naver.com>
2024-10-18 02:13:33 +0900

    feature: 상품 선택 및 장바구니 추가 기능 구현

--- src/advanced/components/carts/ProductSelector.jsx
+++ src/advanced/components/carts/ProductSelector.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+export const ProductSelector = ({ productInventory, setProductInventory, cartList, setCartList, selectedProduct, setSelectedProduct }) => {
+  const handleOptionChange = (lastSelectedProductId) => {
+    if (Array.isArray(productInventory) === true) {
+      const cartItem = productInventory.find((product) => product.id === lastSelectedProductId);
+      setSelectedProduct(cartItem);
+    }
+  };
 
-export const ProductSelector = () => {
+  const handleAddCartButton = () => {
+    const newCartList = cartList.slice();
+
+    const isNewCartItemIndex = cartList.findIndex((item) => item.id === selectedProduct.id) === -1;
+
+    if (isNewCartItemIndex === true) {
+      newCartList.push(selectedProduct);
+      setCartList(newCartList);
+
+      // const newProductInventory = productInventory.slice()
+      // newProductInventory[]
+
+      // setProductInventory(Object.assign({}, productInventory, ))
+    }
+  };
+
   return (
     <>
-      <select id="product-select" className="-2 mr-2 rounded border">
-        <option value="p1">상품1 - 10000원</option>
-        <option value="p2">상품2 - 20000원</option>
-        <option value="p3">상품3 - 30000원</option>
-        <option value="p4">상품4 - 15000원</option>
-        <option value="p5">상품5 - 25000원</option>
+      <select id="product-select" className="-2 mr-2 rounded border" onChange={(e) => handleOptionChange(e.target.value)}>
+        {productInventory.map((product) => (
+          <option key={product.id} value={product.id}>{`${product.name} - ${product.price}`}</option>
+        ))}
       </select>
-      <button
-        id="add-to-cart"
-        className="rounded bg-blue-500 px-4 py-2 text-white"
-      >
+      <button id="add-to-cart" className="rounded bg-blue-500 px-4 py-2 text-white" onClick={handleAddCartButton}>
         추가
       </button>
     </>
