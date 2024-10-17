bb5cb798adea00566eff62c9b2561220ea61efae
Gred <jihong.park.dev@gmail.com>
2024-10-18 00:17:42 +0900

    feat: main 함수 기능 추가

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,25 +1,35 @@
-import CartInfo from '../components/CartInfo';
-import CartItem from '../components/CartItem';
-import HeaderTitle from '../components/headerTitle';
-import ProductSelect from '../components/ProductSelect';
-import TotalPrice from '../components/TotalPrice';
-import StockStatus from '../components/StockStatus';
+import CartInfo from './components/CartInfo';
+import CartAddButton from './components/CartAddButton';
+import HeaderTitle from './components/HeaderTitle';
+import ProductSelect from './components/ProductSelect';
+import TotalPrice from './components/TotalPrice';
+import StockStatus from './components/StockStatus';
+
+import { updateSelectOptions } from './utils/ui';
+import { updateCartInfo } from './utils/cart';
+import { setupIntervals } from './intervals';
+import { setupEventListner } from './eventHandler';
 
 function main() {
   const root = document.getElementById('app');
-
   root.innerHTML = `
     <div class="bg-gray-100 p-8">
       <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
           ${HeaderTitle()}
-          ${CartItem({ id: 1, title: 'testTitle' })}
           ${CartInfo()}
           ${TotalPrice()}
           ${ProductSelect()}
+          ${CartAddButton()}
           ${StockStatus()}
       </div>
     </div>
   `;
+
+  updateSelectOptions();
+  updateCartInfo();
+
+  setupIntervals();
+  setupEventListner();
 }
 
 main();
