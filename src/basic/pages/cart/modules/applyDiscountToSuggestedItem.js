a20101122265d7642cd127caae23d02136a1c22c
윤성용 <iop017779@gmail.com>
2024-10-17 22:46:32 +0900

    refactor : applyDiscountToSuggestedItem 함수 상수 추가

--- src/basic/pages/cart/modules/applyDiscountToSuggestedItem.js
+++ src/basic/pages/cart/modules/applyDiscountToSuggestedItem.js
@@ -1,6 +1,11 @@
 import { DEFAULT_PRODUCT_LIST } from '../constant/defaultProducts.js';
 import { selectedProductItemState } from '../state.js';
 
+const SUGGESTED_DISCOUNT_PERCENTAGE = 0.95;
+
+const INITIAL_DELAY_MS = Math.random() * 20000;
+const INTERVAL_TIME_MS = 60000;
+
 function applyDiscountToSuggestedItem() {
   const { selectedProductItem } = selectedProductItemState.getState();
 
@@ -12,16 +17,13 @@ function applyDiscountToSuggestedItem() {
     (item) => item.id !== selectedProductItem && item.quantity > 0,
   );
   if (suggest) {
-    suggest.price = Math.round(suggest.price * 0.95);
+    suggest.price = Math.round(suggest.price * SUGGESTED_DISCOUNT_PERCENTAGE);
     alert(`${suggest.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`);
   }
 }
 
 export function initSuggestedDiscount() {
-  const initialDelay = Math.random() * 20000;
-  const intervalTime = 60000;
-
   setTimeout(() => {
-    setInterval(applyDiscountToSuggestedItem, intervalTime);
-  }, initialDelay);
+    setInterval(applyDiscountToSuggestedItem, INTERVAL_TIME_MS);
+  }, INITIAL_DELAY_MS);
 }
