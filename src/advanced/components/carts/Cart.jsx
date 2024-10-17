1593596a87421c41f72820b13f01980b44eb30bd
Ensil-dev <lasags@naver.com>
2024-10-18 02:09:02 +0900

    feature: 장바구니 아이템 표시 및 관리 UI 구현

--- src/advanced/components/carts/Cart.jsx
+++ src/advanced/components/carts/Cart.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import { BoxTitle } from '../templates/BoxTitle';
 import { CartItems } from './CartItems';
 import { CartTotal } from './CartTotal';
 import { ProductSelector } from './ProductSelector';
 import { StockStatus } from './StockStatus';
+import { initialProductInventory } from '../../datas/productInventory';
 
 export const Cart = () => {
-    return (
-        <div className='bg-gray-100 p-8'>
-            <div className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8'>
-                <BoxTitle />
-                <CartItems />
-                <CartTotal />
-                <ProductSelector />
-                <StockStatus />
-            </div>
-        </div>
-    );
+  const [productInventory, setProductInventory] = useState(initialProductInventory);
+  const [selectedProduct, setSelectedProduct] = useState(initialProductInventory[0]);
+  const [cartList, setCartList] = useState([]);
+
+  return (
+    <div className="bg-gray-100 p-8">
+      <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white p-8 shadow-md md:max-w-2xl">
+        <BoxTitle />
+        <CartItems cartList={cartList} />
+        <CartTotal cartList={cartList} productInventory={productInventory} />
+        <ProductSelector productInventory={productInventory} setProductInventory={setProductInventory} cartList={cartList} setCartList={setCartList} setSelectedProduct={setSelectedProduct} selectedProduct={selectedProduct} />
+        <StockStatus productInventory={productInventory} />
+      </div>
+    </div>
+  );
 };
--- src/advanced/components/carts/CartItems.jsx
+++ src/advanced/components/carts/CartItems.jsx
@@ -1,33 +1,27 @@
-import React from "react";
+import React from 'react';
 
-export const CartItems = () => {
+export const CartItems = ({ cartList }) => {
+  console.log(cartList[0]);
   return (
     <div id="cart-items">
-      <div id="p1" className="mb-2 flex items-center justify-between">
-        <span>상품1 - 0.2원 x 1</span>
-        <div>
-          <button
-            className="quantity-change mr-1 rounded bg-blue-500 px-2 py-1 text-white"
-            data-product-id="${itemToAdd.id}"
-            data-change="-1"
-          >
-            -
-          </button>
-          <button
-            className="quantity-change mr-1 rounded bg-blue-500 px-2 py-1 text-white"
-            data-product-id="${itemToAdd.id}"
-            data-change="1"
-          >
-            +
-          </button>
-          <button
-            className="remove-item rounded bg-red-500 px-2 py-1 text-white"
-            data-product-id="${itemToAdd.id}"
-          >
-            삭제
-          </button>
-        </div>
-      </div>
+      {cartList.map((cartItem) => {
+        return (
+          <div key={cartItem.id} id={cartItem.id} className="mb-2 flex items-center justify-between">
+            <span>{`${cartItem.name} - ${cartItem.price} x 1`}</span>
+            <div>
+              <button className="quantity-change mr-1 rounded bg-blue-500 px-2 py-1 text-white" data-product-id={cartItem.id} data-change="-1">
+                -
+              </button>
+              <button className="quantity-change mr-1 rounded bg-blue-500 px-2 py-1 text-white" data-product-id={cartItem.id} data-change="1">
+                +
+              </button>
+              <button className="remove-item rounded bg-red-500 px-2 py-1 text-white" data-product-id={cartItem.id}>
+                삭제
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
