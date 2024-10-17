d062eb6fdf241e0c2f31bcdbc9a28aff1ec4650d
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-18 08:25:18 +0900

    ♻️ refactor :: 컴포넌트 폴더 내부 한번에 내보내기 index 파일 구현

---
+++ src/advanced/src/component/index.ts
@@ -0,0 +1,7 @@
+export {AddToCartButton} from "./AddToCartButton";
+export {Button} from './Button'
+export {CartItems} from './CartItems'
+export {CartTotal} from './CartTotal'
+export {Layout} from './Layout'
+export {ProductSelect} from './ProductSelect'
+export {StockStatus} from './StockStatus'
