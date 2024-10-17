0fbff6043801df28bb28e40366ecf85e42390d0f
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-10-17 19:24:49 +0900

    refactor: addBtnEvent 위치 변경

--- src/basic/actions/addBtnEvent.js
+++ src/basic/actions/addBtnEvent.js
@@ -1,9 +1,11 @@
-import { getElementById } from './util/element.js';
-import { SELECT_VIEW_ID } from './const/VIEW_ID.js';
-import { PROD_LIST } from './const/PROD_LIST.js';
-import { SOLD_OUT } from './const/MASIC_NUMBER.js';
+import { appendChildren, createElement, getElementById } from '../../util/element.js';
+import { CART_VIEW_ID, SELECT_VIEW_ID } from '../../const/VIEW_ID.js';
+import { PROD_LIST } from '../../const/PROD_LIST.js';
+import { SOLD_OUT } from '../../const/MASIC_NUMBER.js';
+import calcCart from './calcCart.js';
+import plusPurchaseCount from '../../util/plusPurchaseCount.js';
 
-export function addBtnEvent() {
+export default function addBtnEvent() {
   const selectView = getElementById(SELECT_VIEW_ID);
   const selectedOptionId = selectView.value;
   const selectedProduct = findProductById(selectedOptionId);
@@ -43,27 +45,25 @@ function updateExistingProductInCart(productViewInCart, product) {
 }
 
 function updateProductView(productViewInCart, selectedProduct, purchaseCount) {
-  productViewInCart.querySelector('span').textContent = selectedProduct.name + ' - ' + selectedProduct.val + '원 x ' + purchaseCount;
+  productViewInCart.querySelector('span').textContent = selectedProduct.name + ' - ' + selectedProduct.price + '원 x ' + purchaseCount;
   selectedProduct.quantity--;
 }
 
-function plusPurchaseCount(productViewInCart) {
-  return parseInt(productViewInCart.querySelector('span').textContent.split('x ')[1]) + 1;
-}
-
 function addNewProductToCart(product) {
-  const newProductViewInCart = document.createElement('div');
+  const newProductViewInCart = createElement('div');
 
   createProductViewInCart(newProductViewInCart, product);
-  cartDisp.appendChild(newProductViewInCart);
-  
+
+  const cartView = getElementById(CART_VIEW_ID);
+  appendChildren(cartView, newProductViewInCart);
+
   product.quantity--;
 }
 
 function createProductViewInCart(newProductViewInCart, selectedProduct) {
   newProductViewInCart.id = selectedProduct.id;
   newProductViewInCart.className = 'flex justify-between items-center mb-2';
-  newProductViewInCart.innerHTML = '<span>' + selectedProduct.name + ' - ' + selectedProduct.val + '원 x 1</span><div>' +
+  newProductViewInCart.innerHTML = '<span>' + selectedProduct.name + ' - ' + selectedProduct.price + '원 x 1</span><div>' +
     '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' + selectedProduct.id + '" data-change="-1">-</button>' +
     '<button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="' + selectedProduct.id + '" data-change="1">+</button>' +
     '<button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="' + selectedProduct.id + '">삭제</button></div>';
