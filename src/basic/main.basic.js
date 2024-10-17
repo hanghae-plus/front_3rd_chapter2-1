9f0522c0a4891bf06e15b3cf23f3005285068e0d
i33W <hwangbomee@kakao.com>
2024-10-18 02:21:09 +0900

    fix: 첫 화면 로드 시 옵션과 카트 계산하는 부분 추가

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,13 +1,15 @@
 import renderHome from './components/renderHome';
 import updateSelectOptions from './components/updateSelectOptions';
+import calculateCart from './utils/calculateCart';
 import { activateLuckySale, activateSuggestSale } from './utils/eventManager';
 
 function main() {
   let root = document.getElementById('app');
   root.appendChild(renderHome());
 
-  // 첫 로드 시 select option 업데이트
+  // 화면 처음 로드 시 옵션 설정 & 총액 계산
   updateSelectOptions();
+  calculateCart();
 
   // 랜덤 타이머 이벤트 설정
   activateLuckySale();
