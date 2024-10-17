2e7b4c0b8aba764bbe4f275165239100aab220ac
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-10-17 18:09:55 +0900

    feat: 상수 추가

--- src/const/MASIC_NUMBER.js
+++ src/const/MASIC_NUMBER.js
@@ -1,1 +1,14 @@
 export const SOLD_OUT = 0;
+
+// 10개 이상 30개 미만 구매시 할인율
+export const PROD_FIRST_DISCOUNT_RATIO = 0.1;
+export const PROD_SECOND_DISCOUNT_RATIO = 0.15;
+export const PROD_THIRD_DISCOUNT_RATIO = 0.2;
+export const PROD_FOURTH_DISCOUNT_RATIO = 0.05;
+export const PROD_FIFTH_DISCOUNT_RATIO = 0.25;
+
+// 30개 이상 구매시 할인율
+export const BULK_PURCHASE_DISCOUNT_RATIO = 0.25;
+
+// 화요일 할인율
+export const TUESDAY_DISCOUNT_RATIO = 0.1;
