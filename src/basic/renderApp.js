2b451f0940acf7c224c5b98b0b1aa3ecd5828c2d
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-17 16:32:24 +0900

    ♻️  refactor :: 앱 UI를 초기화하고 구성 요소 반환하는 renderApp로 리팩토링

---
+++ src/basic/renderApp.js
@@ -0,0 +1,28 @@
+import { createTitle } from './createElements';
+import { createCartItemsDiv, createContainer, createWrapper, createCartTotalDiv, createProductSelect, createAddToCartButton, createStockStatusDiv } from "./views/components";
+import { assembleComponents } from "./views/utils";
+
+
+/**
+ * @function renderApp
+ * @description 앱 UI를 초기화하고 구성 요소 반환
+ * 
+ * @returns {HTMLElement} 장바구니 앱 컨테이너 요소
+ */
+
+const renderApp = () => {
+  const container = createContainer();
+  const wrapper = createWrapper();
+  const title = createTitle('장바구니');
+  const cartItemsDiv = createCartItemsDiv();
+  const cartTotalDiv = createCartTotalDiv();
+  const productSelect = createProductSelect();
+  const addToCartButton = createAddToCartButton();
+  const stockStatusDiv = createStockStatusDiv();
+
+  assembleComponents(wrapper, title, cartItemsDiv, cartTotalDiv, productSelect, addToCartButton, stockStatusDiv);
+  container.appendChild(wrapper);
+  return container;
+};
+
+export default renderApp;
