cdc48a50f298963a34169ecd02ec3a9949fd5ad5
Ensil-dev <lasags@naver.com>
2024-10-18 02:09:57 +0900

    feature: 장바구니 총액 및 포인트 표시 UI 구현

--- src/advanced/components/carts/CartTotal.jsx
+++ src/advanced/components/carts/CartTotal.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AMOUNT_PER_LOYALTY_POINT } from '../../constants';
 
-export const CartTotal = () => {
+export const CartTotal = ({ cartList }) => {
+  console.log(cartList);
+
+  const [cartTotalAmount, setCartTotalAmount] = useState(0);
+  const [bonusPoint, setBonusPoint] = useState(0);
+
+  useEffect(() => {
+    let currentCartTotalAmount = 0;
+    cartList.forEach((cart) => {
+      console.log(cart);
+      currentCartTotalAmount += cart.price;
+    });
+    setCartTotalAmount(currentCartTotalAmount);
+    setBonusPoint(currentCartTotalAmount / AMOUNT_PER_LOYALTY_POINT);
+    console.log(`cartTotalAmount: ${cartTotalAmount}`);
+  }, [cartList]);
+
   return (
     <div id="cart-total" className="my-4 text-xl font-bold">
-      총액: 0원
+      총액: {Math.round(cartTotalAmount)}원
       <span id="loyalty-points" className="ml-2 text-blue-500">
-        (포인트: 0)
+        (포인트: {bonusPoint})
       </span>
     </div>
   );
