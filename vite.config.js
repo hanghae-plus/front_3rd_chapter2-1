b47ac61aeb2d87468115d6f74253fb2b5e198c73
KimMyeongSu <kms930508@gmail.com>
2024-10-17 21:31:19 +0900

    react & typeScript Set

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
   },
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
