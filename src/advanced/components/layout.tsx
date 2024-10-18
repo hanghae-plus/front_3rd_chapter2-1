4e73411c7ed8f0c53eb3d95facb5d0b42e6f7662
dongjang <jdwls27@naver.com>
2024-10-18 13:45:40 +0900

    layout 함수들 훅으로 분리

--- src/advanced/components/layout.tsx
+++ src/advanced/components/layout.tsx
@@ -1,79 +1,31 @@
 import React, { useEffect } from 'react';
 import { useAppContext } from '../context/appContext';
-import { useCartUtils } from '../ts/utils/cartUtils';
-import { useRenderUtils } from '../ts/utils/renderUtils';
+import { useCart } from '../hooks/useCart';
 import CartProductList from './CartProductList';
 import CartAmountInfo from './CartAmountInfo';
 import StockInfo from './StockInfo';
 import AddToCart from './AddToCart';
-import ProductSelectList from './productSelectList';
+import { useRender } from '../hooks/useRender';
+import ProductSelectList from './ProductSelectList';
+import { useInterval } from '../hooks/useInterval';
 
 const Layout = () => {
-  const { setProductSelectDropDown, cartProductList, setSelectedProductId, productList, setProductList } =
-    useAppContext();
+  const { setProductSelectDropDown, cartProductList, setSelectedProductId } = useAppContext();
 
-  const { calcCart } = useCartUtils();
-  const { renderProductList } = useRenderUtils();
-
-  //타임 세일
-  const timeSaleProductRenderer = () => {
-    if (productList.length > 0) {
-      const timeSaleProduct = productList[Math.floor(Math.random() * productList.length)];
-
-      if (Math.random() < 0.3 && timeSaleProduct.stock > 0) {
-        timeSaleProduct.price = Math.round(timeSaleProduct.price * 0.8);
-        alert(`번개세일! ${timeSaleProduct.name}이(가) 20% 할인 중입니다!`);
-
-        const updateProductList = productList.map((product) => {
-          if (product.id === timeSaleProduct.id) {
-            return { ...product, price: timeSaleProduct.price };
-          }
-          return product;
-        });
-        setProductList(updateProductList);
-        renderProductList();
-        setProductSelectDropDown(<>{options}</>);
-      }
-    }
-  };
-
-  //상품 제안
-  const productSuggestRender = () => {
-    const cartProductLength = cartProductList.length;
-    if (cartProductLength > 0) {
-      const productSuggest = productList[cartProductLength - 0];
-      alert(`${productSuggest.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`);
-      const updateProductList = productList.map((product) => {
-        if (product.id === productSuggest.id) {
-          return { ...product, price: Math.round(productSuggest.price * 0.95) };
-        }
-        return product;
-      });
-      setProductList(updateProductList);
-      renderProductList();
-      setProductSelectDropDown(<>{options}</>);
-    }
-  };
-
+  const { calcCart } = useCart();
+  const { renderProductList } = useRender();
+  const { timeSaleInterval, suggestInterval } = useInterval();
   const options = renderProductList();
 
   // 타임세일 및 제품 추천 useEffect
   useEffect(() => {
-    const timeSaleInterval = setInterval(() => {
-      timeSaleProductRenderer();
-    }, 30000);
-
-    const suggestInterval = setInterval(() => {
-      productSuggestRender();
-    }, 60000);
-
     return () => {
       clearInterval(timeSaleInterval);
       clearInterval(suggestInterval);
     };
   }, []);
 
-  //select에 값이 세팅이 안 돼 있어서 임의로 지정을 위한 useEffect
+  //select에 값이 세팅이 안 돼 있어서 처음 값을 임의로 지정 위한 useEffect
   useEffect(() => {
     setSelectedProductId('p1');
   }, []);
