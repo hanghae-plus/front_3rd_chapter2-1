0aa1be38a1ba9c61cc49c109c87cdcd8e99fc098
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-10-18 09:54:00 +0900

    feat: 이벤트 함수 추가

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,9 +1,13 @@
 import initView from '../util/view.js';
 import addBtnEvent from './actions/addBtnEvent.js';
+import calcCart from './actions/calcCart.js';
+import addCartEvent from './actions/addCartEvent.js';
 
 function main() {
   initView();
+  calcCart();
   addBtnEvent();
+  addCartEvent();
 }
 
 main();
